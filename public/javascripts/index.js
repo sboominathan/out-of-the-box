cca7a360214e4782a342bd14da26b15cb2473129
Sooraj Boominathan <sooraj.boomi@gmail.com>
2016-08-26 13:12:50 -0500

    code cleanup

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,25 +1,14 @@
 $(document).ready(function(){
    $(window).scroll(function(){
-        if($(this).scrollTop() > 300) {
-            $("nav").css({"background-color":"#1d1d1d"});   
-        	$("#navbar-collapse").css({"background-color":"#1d1d1d"});   
-        	/*$("nav").addClass("scroll"); 
-        	$("#blue").css({"color":"black"});
-        	$("#about").css({"color":"black"});
-        	$("#dates").css({"color":"black"});
-        	$("#goto-faq").css({"color":"black"});*/
-        	//$("#")
+        if($(this).scrollTop() > 450) {
+            $("nav").css({"background-color":"#05354e"});
+            $("#navbar-collapse").css({"background-color":"#05354e"});     
+            
         }
         else{
-        	$("nav").css({"background-color":"#05354e"});
-        	$("#navbar-collapse").css({"background-color":"#05354e"});     
-        	/*$("#navbar-collapse").css({"background-color":"#1d1d1d"});
-        	$("#blue").css({"color":"#99ddff"});
-        	$("#about").css({"color":"rgba(242, 246, 249, 0.25)"});
-        	$("#dates").css({"color":"rgba(242, 246, 249, 0.25)"});
-        	$("#goto-faq").css({"color":"rgba(242, 246, 249, 0.25)"});*/
+        	$("nav").css({"background-color":"#1d1d1d"});   
+            $("#navbar-collapse").css({"background-color":"#1d1d1d"});   
         }
     })
     
-
 })
