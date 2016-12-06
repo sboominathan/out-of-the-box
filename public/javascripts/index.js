a43b61d1804cab856e8489ca714862b2814035e0
Tiffany Pan <tpan@mit.edu>
2016-12-05 22:22:50 -0500

    modifications for new splash page design

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
    $(window).scroll(function(){
-        if($(this).scrollTop() > 450) {
-            $("nav").css({"background-color":"#05354e"});
-            $("#navbar-collapse").css({"background-color":"#05354e"});     
-            
+        if($(this).scrollTop() < 620) {
+            $("nav").css({"background-color":"#0f1428"});
+            $("#navbar-collapse").css({"background-color":"#0f1428"});
         }
+        else if($(this).scrollTop() > 620 &&  $(this).scrollTop() < 1400) {
+            $("nav").css({"background-color":"#b1264e"});
+            $("#navbar-collapse").css({"background-color":"#b1264e"});
+        }
+        else if($(this).scrollTop() > 1440 &&  $(this).scrollTop() < 3250) {
+            $("nav").css({"background-color":"#80c3d8"});
+            $("#navbar-collapse").css({"background-color":"#80c3d8"});
+        }
+        else if($(this).scrollTop() > 3250 &&  $(this).scrollTop() < 4000) {
+            $("nav").css({"background-color":"#f5b634"});
+            $("#navbar-collapse").css({"background-color":"#f5b634"});
+        }  
+        else if($(this).scrollTop() > 4000 &&  $(this).scrollTop() < 4800) {
+            $("nav").css({"background-color":"#b1264e"});
+            $("#navbar-collapse").css({"background-color":"#b1264e"});
+        }         
         else{
-        	$("nav").css({"background-color":"#1d1d1d"});   
-            $("#navbar-collapse").css({"background-color":"#1d1d1d"});   
+        	$("nav").css({"background-color":"#0f1428"});   
+            $("#navbar-collapse").css({"background-color":"#0f1428"});   
         }
     })
     
