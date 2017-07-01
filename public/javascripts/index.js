cfa89d09bf753daf4b19c27da0a582a7c875d4da
Sooraj Boominathan <soorajb@mit.edu>
2017-06-30 21:50:28 -0700

    merged in Ramya and Tiffany's changes for new design

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,46 @@
-$(document).ready(function(){
-   $(window).scroll(function(){
-        if($(this).scrollTop() > 450) {
-            $("nav").css({"background-color":"#05354e"});
-            $("#navbar-collapse").css({"background-color":"#05354e"});     
+// $(document).ready(function(){
+//    $(window).scroll(function(){
+//         if($(this).scrollTop() > 450) {
+//             $("nav").css({"background-color":"#05354e"});
+//             $("#navbar-collapse").css({"background-color":"#05354e"});     
             
+//         }
+//         else{
+//         	$("nav").css({"background-color":"#1d1d1d"});   
+//             $("#navbar-collapse").css({"background-color":"#1d1d1d"});   
+//         }
+//     })
+    
+// })
+
+$(document).ready(function(){
+
+   $(window).scroll(function(){
+        var scrollPercent = ($(this).scrollTop() / $(document).height()) * 100;
+
+        if(scrollPercent < 11) {
+            $("nav").css({"background-color":"#0f1428"});
+            $("#navbar-collapse").css({"background-color":"#0f1428"});
         }
+        else if(scrollPercent > 10 && scrollPercent < 28) {
+            $("nav").css({"background-color":"#b1264e"});
+            $("#navbar-collapse").css({"background-color":"#b1264e"});
+        }
+        else if(scrollPercent > 28 && scrollPercent < 60) {
+            $("nav").css({"background-color":"#80c3d8"});
+            $("#navbar-collapse").css({"background-color":"#80c3d8"});
+        }
+        else if(scrollPercent > 60 && scrollPercent < 75) {
+            $("nav").css({"background-color":"#f5b634"});
+            $("#navbar-collapse").css({"background-color":"#f5b634"});
+        }  
+        else if(scrollPercent > 75 && scrollPercent < 90) {
+            $("nav").css({"background-color":"#b1264e"});
+            $("#navbar-collapse").css({"background-color":"#b1264e"});
+        }         
         else{
-        	$("nav").css({"background-color":"#1d1d1d"});   
-            $("#navbar-collapse").css({"background-color":"#1d1d1d"});   
+            $("nav").css({"background-color":"#0f1428"});   
+            $("#navbar-collapse").css({"background-color":"#0f1428"});   
         }
     })
     
