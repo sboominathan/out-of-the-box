5f5635b4dc316a96a5cf9c637ebb9c840b627d15
Sooraj Boominathan <soorajb@mit.edu>
2017-06-30 21:50:53 -0700

    cleaned up js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,18 +1,3 @@
-// $(document).ready(function(){
-//    $(window).scroll(function(){
-//         if($(this).scrollTop() > 450) {
-//             $("nav").css({"background-color":"#05354e"});
-//             $("#navbar-collapse").css({"background-color":"#05354e"});     
-            
-//         }
-//         else{
-//         	$("nav").css({"background-color":"#1d1d1d"});   
-//             $("#navbar-collapse").css({"background-color":"#1d1d1d"});   
-//         }
-//     })
-    
-// })
-
 $(document).ready(function(){
 
    $(window).scroll(function(){
